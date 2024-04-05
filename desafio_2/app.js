2f1111f6589d6b83917451b37606742f32191ebc
Cecilia Tarsia <ceciliatarsia@gmail.com>
2024-04-05 18:33:31 -0300

    Actualizados métodos Update y Delete

--- desafio_2/app.js
+++ desafio_2/app.js
@@ -1,8 +1,28 @@
 const ProductManager = require('./src/ProductManager.js')
-const productManager = new ProductManager()
+const manager = new ProductManager()
 
 // TEST DESAFIO_2
 
-productManager.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
-console.log(productManager.getProducts())
-productManager.getProductById(1)    
+manager.addProduct("Producto Uno", "Este es un producto 1", 200, "Sin imagen", "abc123", 25)
+manager.addProduct("Producto Dos", "Este es un producto 2", 150, "Sin imagen", "abc456", 30)
+
+
+async function getAllProducts(){
+    const productList = await manager.getProducts();
+    console.log(productList);
+}
+
+getAllProducts()
+
+// async function getProductById2(id){
+//     const productFound = await manager.getProductById(id)
+//     console.log(productFound)
+// }
+
+// getProductById2(1)
+
+manager.getProductById(1)
+manager.updateProduct(1, {price: 300})
+manager.deleteProduct(2)
+getAllProducts()
+
