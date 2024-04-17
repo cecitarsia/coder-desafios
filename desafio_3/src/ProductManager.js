1a1ea69dd2308f88f4ea553afb2fa56ab2ab1daf
Cecilia Tarsia <ceciliatarsia@gmail.com>
2024-04-16 23:48:01 -0300

    Desafio 3

---
+++ desafio_3/src/ProductManager.js
@@ -0,0 +1,114 @@
+// DESAFÍO #3
+const fs = require('fs').promises;
+
+class ProductManager {
+    constructor() {
+        this.products = []
+        this.path = "Products.json"
+    }
+
+
+    async addProduct(title, description, price, thumbnail, code, stock) {
+        try {
+            const codeExists = this.products.find((product) => product.code === code)
+            const productIsValid = title && description && price && thumbnail && code && stock !== undefined
+            
+            if (!productIsValid) {
+                console.log("No se pudo agregar el producto, todos los campos son obligatorios.")
+                return;
+            } else if (codeExists) {
+                console.log("No se pudo agregar el producto, el código ya existe.")
+                return;
+            } else {
+                const id = this.products.length + 1
+                const product = {
+                    id: id,
+                    title,
+                    description,
+                    price,
+                    thumbnail,
+                    code,
+                    stock,
+                }
+                this.products.push(product)
+                await fs.writeFile(this.path, JSON.stringify(this.products, null, 2));
+                console.log("Producto agregado.")
+            }
+        } catch (error) {
+            console.error("Error al agregar producto", error);
+        }
+    }
+
+    async getProducts() {
+        try {
+            return await this.readProducts()
+        } catch (error) {
+            console.error("Error al consultar productos", error);
+            return []
+        }
+    }
+
+    async readProducts() {
+        try {
+            const data = await fs.readFile(this.path, 'utf8')
+            return JSON.parse(data)
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                return []
+            } else {
+                throw error
+            }
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const products = await this.getProducts()
+            const productFound = products.find((product) => product.id === id)
+            if (!productFound) {
+                console.log("El producto buscado no existe.")
+            }
+            return productFound;
+        } catch (error) {
+            console.error("ID de producto no encontrado", error);
+        }
+    }
+
+    async updateProduct(id, value) {
+        try {
+            const products = await this.readProducts()
+            // const index = products.indexOf(p => p.id === id) //--> No entiendo xq no funciona este
+            const index = products.findIndex(p => p.id === id)
+            if(index !== -1) {
+                products[index] = { ...products[index], ...value};
+                await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+            }else{
+                console.log("No se encontro el ID del producto a actualizar");
+            }
+
+        } catch (error) {
+            console.error("Error al actualizar el producto", error);
+        }
+    }
+
+    async deleteProduct(id) {
+        try {
+            const products = await this.readProducts()
+            const index = products.findIndex(p => p.id === id)
+
+            if(index !== -1) {
+                const newProductList = products.filter(p => p.id != id)
+                await fs.writeFile(this.path, JSON.stringify(newProductList, null, 2))
+            }else{
+                console.log("No se encontro el ID del producto a remover");
+            }
+
+        } catch (error) {
+            console.log('No se pudo borrar el producto')  
+        }
+    }
+}
+
+
+
+module.exports = ProductManager
---
+++ desafio_3/src/app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+const ProductManager = require('./ProductManager.js')
+const manager = new ProductManager('./Products.json')
+
+manager.addProduct("Producto Uno", "Este es un producto 1", 200, "Sin imagen", "abc101", 25)
+manager.addProduct("Producto Dos", "Este es un producto 2", 150, "Sin imagen", "abc102", 30)
+manager.addProduct("Producto Tres", "Este es un producto 3", 450, "Sin imagen", "abc103", 54)
+manager.addProduct("Producto Cuatro", "Este es un producto 4", 240, "Sin imagen", "abc104", 23)
+manager.addProduct("Producto Cinco", "Este es un producto 5", 130, "Sin imagen", "abc105", 45)
+manager.addProduct("Producto Seis", "Este es un producto 6", 260, "Sin imagen", "abc106", 86)
+manager.addProduct("Producto Siete", "Este es un producto 7", 50, "Sin imagen", "abc107", 35)
+manager.addProduct("Producto Ocho", "Este es un producto 8", 230, "Sin imagen", "abc108", 23)
+manager.addProduct("Producto Nueve", "Este es un producto 9", 27, "Sin imagen", "abc109", 86)
+manager.addProduct("Producto Diez", "Este es un producto 10", 45, "Sin imagen", "abc110", 24)
+
+
+async function getAllProducts(){
+    const productList = await manager.getProducts();
+    console.log(productList);
+}
+getAllProducts()
+
+
+/////// DESAFIO_3 //////////
+
+//Middlewares 
+app.use(express.json())
+app.use(express.urlencoded({extended:true})) // => para analizar el cuerpo de las solicitudes y para trabajar con queries y params
+
+
+//Endpoints
+
+// Get all products with a limit
+app.get('/products', async (req, res) => {
+    let limit = parseInt(req.query.limit)
+    
+    try {
+        const products = await manager.getProducts()
+        let limitedProducts = [...products]
+
+        if (!isNaN(limit) && limit > 0) {
+            limitedProducts = limitedProducts.slice(0, limit)
+            res.json(limitedProducts)
+        }
+        res.json(products)
+    } catch (error) {
+        res.status(404).json({ message: "Error 404 - Productos no encontrados" })
+    }
+
+})
+
+// Get one product by ID
+app.get("/products/:pid", async (req, res) => {
+
+    try {
+        let pid = parseInt(req.params.pid);
+        const product = await manager.getProductById(pid);
+        if (product) {
+            res.json(product);
+        } 
+    } catch (error) {
+        res.status(404).json({ message: "Error 404 - Producto no encontrado." })
+    }
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
+
+
+
+
+
+
+
