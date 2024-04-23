ce92b8632c194c54f098350199a258db6d6da49b
Cecilia Tarsia <ceciliatarsia@gmail.com>
2024-04-23 14:35:17 -0300

    .

--- pre-entrega-1/src/app.js
+++ pre-entrega-1/src/app.js
@@ -1,12 +1,9 @@
-const fs = require('fs').promises
 const express = require("express")
-const path = require("path")
 const app = express()
 const PORT = 8080
 const productsRouter = require("./routes/products.route.js")
 const cartsRouter = require("./routes/carts.route.js")
-const ProductManager = require('./managers/ProductManager.js')
-const productManager = new ProductManager(`${__dirname}/products.json`)
+
 
 
 //Middlewares 
@@ -15,7 +12,6 @@ app.use(express.urlencoded({extended:true}))
 // app.use(express.static(path.join(__dirname, 'public')))
 app.use("/", productsRouter)
 app.use("/", cartsRouter)
-
 
 
 
